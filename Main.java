c82fa18ee3534f62eb962ea6b1d74a0d5bbc00f1
Nolan McCormick <nolan.paul.mccormick@gmail.com>
2025-02-13 09:21:13 -0800

    removed classes

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-// Main switches between players with the Player method, who guess with the Guess method.
+        Player p1 = new Player();
+        Player p2 = new Player();
+        // Main switches between players with the Player method, who guess with the
+        // Guess method.
     }
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,5 @@
 public class Player {
     public static void main(String[] args) {
-
+        
     }
 }
